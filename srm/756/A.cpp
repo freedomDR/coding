b551476edb4a53750322a63152bf290f1a6769bc
freedomDR <zjrforeverdr@gmail.com>
2019-04-25 20:02:43 +0800

    only A

---
+++ srm/756/A.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+class ClinderellaGirls
+{
+public:
+    int count(vector<int> t, vector<int> s)
+    {
+            vector<pair<int, int>> ts(t.size(), make_pair(0, 0));
+            for(int i = 0; i < t.size(); i++)
+            {
+                ts[i].first = t[i];
+                ts[i].second = s[i];
+            }
+            sort(ts.begin(), ts.end(), [](auto ts1, auto ts2){return ts1.first < ts2.first || (ts1.first == ts2.first &&  ts1.second < ts2.second);});
+            int ans = 0;
+            vector<int> left(ts.size(), INT_MIN), right(ts.size(), INT_MIN);
+            int maxx = INT_MIN, maxx1 = INT_MIN;
+            for(int i = t.size()-2; i >= 0; i--)
+            {
+                maxx = max(maxx, ts[i+1].second);
+                maxx1 = max(maxx1, ts[i+1].first);
+                right[i] = maxx;
+                left[i] = maxx1;
+            }
+            left[t.size()-1] = ts[t.size()-1].first;
+            right[t.size()-1] = ts[t.size()-1].second;
+            for(int i = 0; i < t.size(); i++)
+            {
+                if(left[i] == ts[i].first || right[i] <= ts[i].second) ans++;
+                else if()
+            }
+            return ans;
+    }
+};
+
+
+int main()
+{
+    ClinderellaGirls* app = new ClinderellaGirls();
+    vector<int> t{46}, s{81};
+    cout<<app->count(t, s)<<endl;
+    delete app;
+    return 0;
+}
