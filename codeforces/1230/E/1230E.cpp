e436766cc5b945e50ea47574b392869b1cc40acb
freedomDR <zjrforeverdr@gmail.com>
2019-09-26 16:16:52 +0800

    codeforces

--- codeforces/1230/E/1230E.cpp
+++ codeforces/1230/E/1230E.cpp
@@ -1,18 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-ll x[100100], ans, MOD=1e10+7;
-vector<vector<int>> edge(100100);
+ll x[100100], ans, MOD=ll(1e9+7);
+vector<vector<ll>> edge(100100);
 int n;
 
-void dfs(int cur, int pre, unordered_map<ll, int> pre_map)
+void dfs(int cur, int pre, const unordered_map<ll, int>& pre_map)
 {
     unordered_map<ll, int> map;
     for(auto &it:pre_map)
         map[__gcd(it.first, x[cur])] += it.second;
     map[x[cur]]++;
     for(auto &it:map)
+    {
         ans += ((it.first%MOD) * (it.second%MOD))%MOD;
+        ans %= MOD;
+    }
+    /* cout << ans << endl; */
     for(auto &it:edge[cur])
         if(it != pre)
             dfs(it, cur, map);
