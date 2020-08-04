0b6fdfed2a0e4ae48d625918bf2bc5b903c21dc7
董睿 <dongrui05@dongrui05.local>
2020-08-04 17:05:20 +0800

    oh

---
+++ nowcoder/zice_8_A.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main()
+{
+    int n, k; cin >> n >> k;
+    int minn = 1e9, maxx = 0, ans = 0;
+    int tmp = 0;
+    vector<int> arr(n);
+    for(int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+        tmp += arr[i];
+        if(i >= k)
+        {
+            tmp -= arr[n-i];
+        }
+        if(i >= k)
+        {
+            minn = min(minn, tmp);
+            maxx = max(maxx, tmp);
+            ans = max(ans, maxx-minn);
+        }
+    }
+    double ret = ans*1.0/k;
+    cout << ans << endl;
+    cout << ret << endl;
+    return 0;
+}
