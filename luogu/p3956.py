d7ea2893b19bae0553dd12d9fffad7ed4ba71c6b
freedomDR <1640145602@qq.com>
2021-07-03 02:05:16 +0800

    bfs

--- luogu/p3956.py
+++ luogu/p3956.py
@@ -1,10 +1,10 @@
-from collections import deque
+import heapq
 
 n, t = map(int, input().split())
 
 # chess board
 cb = [[0]*n for i in range(n)]
-cost = [[-1]*n for i in range(n)]
+cost = [[1e9]*n for i in range(n)]
 
 for i in range(t):
     x, y, v = map(int, input().split())
@@ -13,26 +13,37 @@ for i in range(t):
     v += 1
     cb[x][y] = v
 
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
+dx = [-1, 0, 1, 0, -2, -1, 0, 1, 2, 1, 0, -1]
+dy = [0, 1, 0, -1, 0, 1, 2, 1, 0, -1, -2, -1]
 
-cost[0][0] = 0
-d = deque()
-d.append((0, 0))
+def bfs():
+    pq = []
+    heapq.heappush(pq, (0, 0, 0))
+    cost[0][0] = 0
+    while(len(pq)>0):
+        c, x, y = heapq.heappop(pq)
+        for i in range(len(dx)):
+            nx, ny = x+dx[i], y+dy[i]
+            if(nx<0 or nx>=n or ny<0 or ny>=n):
+                continue
+            new_c = c
+            if(i >= 4):
+                new_c += 2
+            if(cb[nx][ny]==0):
+                continue
+            if(cb[nx][ny]!=cb[x][y]):
+                new_c += 1
+            if(cost[nx][ny] > new_c):
+                cost[nx][ny] = new_c
+                heapq.heappush(pq, (new_c, nx, ny))
 
-while(len(d)>0):
-    x, y = d.popleft()
-    for i in range(len(dx)):
-        nx, ny = x+dx[i], y+dy[i]
-        if(nx<0 or nx==n or ny<0 or ny==n):
-            continue 
-        if(cb[nx][ny]==cb[x][y] and cb[x][y]==0):
-            continue 
-        if(cb[nx][ny]==cb[x][y]):
-            cost[nx][ny] = cost[x][y]
-        elif(cb[nx][ny]==0):
-            cost[nx][ny] = cost[x][y]+2
-        else:
-            cost[nx][ny] = cost[x][y]+1
+bfs()
+if(cb[n-1][n-1]==0):
+    c1 = cost[n-2][n-1]+2
+    c2 = cost[n-1][n-2]+2
+    cost[n-1][n-1] = min(c1, c2)
+
+if(cost[n-1][n-1] >= 1e9):
+    cost[n-1][n-1] = -1
 
 print(cost[n-1][n-1])
