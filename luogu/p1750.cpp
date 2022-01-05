8218075a7ebf9640752351625a8514f96bc638cf
freedomDR <1640145602@qq.com>
2022-01-05 07:39:12 +0000

    update

---
+++ luogu/p1750.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n, c; cin >> n >> c;
+    vector<int> arr(n), ans;
+    for(auto &v:arr) cin >> v;
+
+    for(auto &v:ans) cout << v << " ";
+    cout << endl;
+    return 0;
+}
