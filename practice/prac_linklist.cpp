5c99ee9c00e1f72855222f3a093d5094194f78b0
freedomDR <1640145602@qq.com>
2023-11-08 11:44:32 +0000

    update

--- practice/prac_linklist.cpp
+++ practice/prac_linklist.cpp
@@ -8,24 +8,20 @@ typedef struct InternalNode
     InternalNode(int val_): val(val_) {}
 }Node;
 
-Node* initLinkList()
-{
-    Node *head = new Node(-1);
-    return head;
-}
-
-void singleLinkListAdd(Node *head, int v)
+Node* singleLinkListAdd(Node *head, int v)
 {
     if(head == nullptr)
     {
-        std::cerr << "head is nullptr" << std::endl;
-        return;
+        head = new Node(v);
+        return head;
     }
+    Node *ori_head = head;
     while(head->next != nullptr)
     {
         head = head->next;
     }
     head->next = new Node(v);
+    return ori_head;
 }
 
 void printLinkList(Node *head)
@@ -60,14 +56,43 @@ void removeNode(Node *head, Node *node)
     delete node;
 }
 
+Node* reverse(Node *head)
+{
+    if(head==nullptr||head->next==nullptr) return head;
+    Node *pre = head;
+    head = head->next;
+    pre->next = nullptr;
+    while(head!=nullptr)
+    {
+        Node *next_node = head->next;
+        head->next = pre;
+        pre = head;
+        head = next_node;
+    }
+    return pre;
+}
+
+Node* reverseV2(Node *node)
+{
+    if(node==nullptr||node->next==nullptr)
+        return node;
+    Node *last_node = reverseV2(node->next);
+    node->next->next = node;
+    node->next = nullptr;
+    return last_node;
+}
+
 void testLinkList()
 {
     std::cout << "Test single link list" << std::endl;
-    Node *head = initLinkList();
-    std::cout << "head->val: " << head->val << "\nhead->next: " << head->next << std::endl;
+    Node *head = nullptr;
     for(int i = 1; i < 10; i++)
-        singleLinkListAdd(head, i);
+        head = singleLinkListAdd(head, i);
     removeNode(head, head->next);
+    printLinkList(head);
+    head = reverse(head);
+    printLinkList(head);
+    head = reverseV2(head);
     printLinkList(head);
     deleteLinkList(head);
 }
