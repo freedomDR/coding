55ca468cee222b61df03b20632f447c16d59fb8d
freedomDR <1640145602@qq.com>
2025-04-02 15:51:45 +0000

    update

---
+++ leetcode/206.反转链表.cpp
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode.cn id=206 lang=cpp
+ *
+ * [206] 反转链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+// struct ListNode {
+//     int val;
+//     ListNode *next;
+//     ListNode() : val(0), next(nullptr) {}
+//     ListNode(int x) : val(x), next(nullptr) {}
+//     ListNode(int x, ListNode *next) : val(x), next(next) {}
+// };
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* cur = head;
+        while (cur) {
+            ListNode* next = cur->next; // 先保存下一个节点
+            cur->next = prev;           // 反转当前节点的指针
+            prev = cur;                // 移动 prev 和 cur 指针
+            cur = next;
+        }
+        return prev; // prev 是新的头节点
+    }
+};
+// @lc code=end
+
